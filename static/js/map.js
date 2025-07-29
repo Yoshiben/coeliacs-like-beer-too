f1fab6daa5f5e1b8c8e508edd5f63774c03fb2a6
Yoshiben <48318488+Yoshiben@users.noreply.github.com>
2025-07-29 12:07:04 +0100

    removed pulse from map

--- static/js/map.js
+++ static/js/map.js
@@ -22,8 +22,8 @@ export const MapModule = (function() {
         maxZoom: 19,
         pubMarkerRadius: 8,
         userMarkerRadius: 8,
-        markerPulseDuration: 1000, // ms
-        markerPulseCount: 4,
+        // markerPulseDuration: 1000, // ms
+        // markerPulseCount: 4,
         tileLayer: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
         attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     };
@@ -1071,12 +1071,12 @@ export const MapModule = (function() {
             window.fullUKMapUserMarker = userMarker;
             // Bring to front after animation completes (2 seconds for 2 pulses)
             // Remove pulsing class and bring to front after animation
-            setTimeout(() => {
-               if (window.fullUKMapUserMarker && window.fullUKMapUserMarker._path) {
-                   L.DomUtil.removeClass(window.fullUKMapUserMarker._path, 'pulsing-marker');
-                   window.fullUKMapUserMarker.bringToFront();
-               }
-            }, config.markerPulseDuration * config.markerPulseCount);
+            // setTimeout(() => {
+            //    if (window.fullUKMapUserMarker && window.fullUKMapUserMarker._path) {
+            //        L.DomUtil.removeClass(window.fullUKMapUserMarker._path, 'pulsing-marker');
+            //        window.fullUKMapUserMarker.bringToFront();
+            //    }
+            // }, config.markerPulseDuration * config.markerPulseCount);
         }
         
         // Add legend (permanent, no toggle)
