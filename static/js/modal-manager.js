b3f93dd66e40605df29f0348a244114992e995bb
Yoshiben <48318488+Yoshiben@users.noreply.github.com>
2025-08-07 14:16:04 +0100

    added nickname modal to mm.js

--- static/js/modal-manager.js
+++ static/js/modal-manager.js
@@ -43,6 +43,7 @@ export const ModalManager = (() => {
         beerListModal: { type: 'modal', group: 'pub-sub', exclusive: true },
         breweryBeersModal: { type: 'modal', group: 'brewery', exclusive: true },
         manualPubEntryModal: { type: 'modal', group: 'form', exclusive: true },
+        nicknameModal: { type: 'modal', group: 'user', exclusive: true },
         
         // Status modals (can stack in specific order)
         gfStatusModal: { type: 'modal', group: 'status', stackable: true, order: 1, priority: true },
